e2a1e3c7e4e7ab9900fff0977f953fe066db0d7b
Meepen <meepdarknessmeep@gmail.com>
2018-07-17 21:42:01 -0400

    increase wait time and add failure retrying

--- queue.js
+++ queue.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
 
+const wait_time_ms = 1250;
+const retries = 3;
+
 let queue = module.exports;
 
 let q = [],
     token;
 
 
-const get_timeout = function get_timeout() {
-    let data = q.pop();
+const get_timeout = function get_timeout(prev) {
+    let data = prev || q.pop();
 
     debug(`GET ${data[0]}`);
 
@@ -17,20 +20,25 @@ const get_timeout = function get_timeout() {
         token = undefined;
 
         if (q.length > 0) 
-            token = setTimeout(get_timeout, 1100);
+            token = setTimeout(get_timeout, wait_time_ms);
     }).catch(e => {
+        if (!prev || prev[2]++ > retries) {
+            token = setTimeout(get_timeout, wait_time_ms, data);
+            debug("retrying previous url...");
+            return;
+        }
         data[1](e, null);
 
         token = undefined;
 
         if (q.length > 0) 
-            token = setTimeout(get_timeout, 1100);
+            token = setTimeout(get_timeout, wait_time_ms);
     });
 }
 
 
 queue.get = function get(url, fn) {
-    q.push([url, fn]);
+    q.push([url, fn, 0]);
     if (!token)
-        token = setTimeout(get_timeout, 1100);
+        token = setTimeout(get_timeout, wait_time_ms);
 }
